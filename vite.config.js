879cd53f915379d589c67adeaa1ca6c34ba9ef0a
lucaslighthouse <lucas.souza@lighthouse.com.br>
2023-03-20 15:42:59 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
-
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        vue(),
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
