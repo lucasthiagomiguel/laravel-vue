b99759d945b1141aa744f1895a31f3f27b33ed81
lucaslighthouse <lucas.souza@lighthouse.com.br>
2023-03-20 17:04:11 -0300

    login fornt

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
+
 
 export default defineConfig({
+    base: "/",
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
         vue({
             template: {
                 transformAssetUrls: {
                     base: null,
-                    includeAbsolute: false,
-                },
-            },
+                    includeAbsolute: false
+                }
+            }
         }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
+    // resolve:{
+    //     alias:{
+    //         "": Path2D.join(__dirname, "/node_modules")
+    //     }
+    // }
 });
